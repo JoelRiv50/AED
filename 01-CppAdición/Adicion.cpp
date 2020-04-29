d90af7f637c9a82389f22a7bbf0fa5fd5d2e776f
JoelRiv50 <63814155+JoelRiv50@users.noreply.github.com>
2020-04-29 22:56:04 +0200

    Add files via upload

---
+++ 01-CppAdición/Adicion.cpp
@@ -0,0 +1,23 @@
+/*
+Programa: adición 
+Objetivo: Pedir al usuario 2 valores y mostrar la suma por pantalla
+Autor: Joel Rivero Torrez
+Ultima modificación: 20/04/2020  
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a;
+	cin>>a;
+	
+	int b;
+	cin>>b;
+	
+	cout<<"La suma es: "<<a+b;
+	 
+    cin.get();
+    return 0;   
+}
